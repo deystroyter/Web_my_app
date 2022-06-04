afbd4b977f914f3a4d4f3bed4dbfc53ed9a554c7
deystroyter <fedorov.andreyka@yandex.ru>
2022-06-05 02:24:16 +0300

    DSAD

--- db/migrate/20220408141306_update_users.rb
+++
@@ -1,7 +0,0 @@
-class UpdateUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :password_digest, :string
-    add_column :users, :remember_token, :string
-    add_index :users, :remember_token
-  end
-end
